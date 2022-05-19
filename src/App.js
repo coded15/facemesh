56a553113526c77e681a26245b9c5c5d0a5ae3a4
coded15 <91874598+coded15@users.noreply.github.com>
2022-05-19 12:03:07 +0530

    loaded facemesh function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 
 // import logo from './logo.svg';
 import './App.css';
@@ -11,32 +11,44 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  //Load facemesh
+  const runFacemsh = async () => {
+    const net = await facemesh.load({
+      inputResolution: { width: 640, height: 480 }, scale: 0.8
+    })
+  }
+
   return (
     <div className="App">
-      <Webcam 
-      ref = {webcamRef} 
-      style={{
-        position: "absolute",
-        marginLeft: "auto",
-        marginRight: "auto",
-        left : 0,
-        right : 0,
-        textAlign: "centr",
-        zIndex: 9,
-        width: 640,
-        height: 480
-      }} />
-      <canvas 
-      ref={canvasRef}
-      style = {{position: "absolute",
-      marginLeft: "auto",
-      marginRight: "auto",
-      left : 0,
-      right : 0,
-      textAlign: "centr",
-      zIndex: 9,
-      width: 640,
-      height: 480}}/>
+      <header className='App-header'>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "centr",
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }} />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "centr",
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }} />
+
+      </header>
 
     </div>
   );
